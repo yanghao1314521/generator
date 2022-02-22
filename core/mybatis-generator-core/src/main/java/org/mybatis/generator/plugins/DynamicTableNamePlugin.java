8fd41ed9625558dd2aaae93119346018ae2cc167
yanghao_ac <yanghao_ac@si-tech.com.cn>
2022-02-22 11:37:43 +0800

    动态表名 生成sql插件 2%

---
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/plugins/DynamicTableNamePlugin.java
@@ -0,0 +1,43 @@
+/*
+ *    Copyright 2006-2022 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.generator.plugins;
+
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import java.util.List;
+
+/**
+ * 动态表名 生成sql
+ *
+ * @author yanghao
+ */
+public class DynamicTableNamePlugin extends PluginAdapter {
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+
+    @Override
+    public boolean providerGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        System.out.println(topLevelClass);
+        System.out.println(introspectedTable);
+//        System.out.println(introspectedTable.getmyba);
+        return super.providerGenerated(topLevelClass, introspectedTable);
+    }
+}
