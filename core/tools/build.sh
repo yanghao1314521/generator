8f8f909d0c2159f526cee15ca6c8fbd3200c18da
yanghao <791475014qq.com>
2023-04-10 21:47:37 +0800

    命令脚本

--- core/tools/build.sh
+++ core/tools/build.sh
@@ -1,4 +1,21 @@
+#
+#    Copyright 2006-2022 the original author or authors.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
 
 
-#rm -r ./java/*
-java -jar ../mybatis-generator-core/target/mybatis-generator-core-1.4.1-SNAPSHOT.jar -configfile generatorConfig_mysql.xml -overwrite
+
+rm -r ./java/*
+
+java -jar ../mybatis-generator-core/target/mybatis-generator-core-1.4.2-SNAPSHOT.jar -configfile generatorConfig_mysql8_0.xml -overwrite
